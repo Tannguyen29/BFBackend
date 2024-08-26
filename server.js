671c6ba819437afb0ce75705f47df83c4b59eb3b
Tannguyen29 <tannvgdh210106@fpt.edu.vn>
2024-08-26 18:30:16 +0700

    banner

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const transporter = require('./config/nodemailer');
 
 const User = require('./models/user');
 const auth = require('./middleware/auth');
+const Banner = require('./models/banner.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -427,6 +428,87 @@ app.get('/plans/:id', async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+// Create a new banner
+app.post('/banners', upload.single('image'), async (req, res) => {
+  try {
+    let imageUrl = req.body.imageUrl;
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'banner'
+      });
+      imageUrl = result.secure_url;
+    }
+    const banner = new Banner({
+      name: req.body.name,
+      imageUrl: imageUrl,
+      expiryDate: new Date(req.body.expiryDate)
+    });
+    await banner.save();
+    res.status(201).send(banner);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Update a banner
+app.put('/banners/:id', upload.single('image'), async (req, res) => {
+  try {
+    const banner = await Banner.findById(req.params.id);
+    if (!banner) {
+      return res.status(404).send();
+    }
+    
+    if (req.body.name) banner.name = req.body.name;
+    if (req.body.expiryDate) banner.expiryDate = new Date(req.body.expiryDate);
+    
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'banner' 
+      });
+      banner.imageUrl = result.secure_url;
+    } else if (req.body.imageUrl) {
+      banner.imageUrl = req.body.imageUrl;
+    }
+
+    await banner.save();
+    res.send(banner);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Get all banners
+app.get('/banners', async (req, res) => {
+  try {
+    const banners = await Banner.find({});
+    res.send(banners);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// Delete a banner
+app.delete('/banners/:id', async (req, res) => {
+  try {
+    const banner = await Banner.findByIdAndDelete(req.params.id);
+    if (!banner) {
+      return res.status(404).send();
+    }
+    res.send(banner);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+// Add this function to automatically remove expired banners
+const removeExpiredBanners = async () => {
+  const currentDate = new Date();
+  await Banner.deleteMany({ expiryDate: { $lt: currentDate } });
+};
+
+// Run this function every day at midnight
+setInterval(removeExpiredBanners, 24 * 60 * 60 * 1000);
 
 // Start server
 app.listen(PORT, () => {
